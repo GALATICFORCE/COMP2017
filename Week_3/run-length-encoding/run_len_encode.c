7c6dd7462905bb7aaeef2c8fce63ab71fe60b244
Kunyu Guo <kguo0343@uni.sydney.edu.au>
2026-03-14 23:11:24 +1100

    first commit

--- Week_3/run-length-encoding/run_len_encode.c
+++ Week_3/run-length-encoding/run_len_encode.c
@@ -1,8 +1,26 @@
+#include <stdio.h>
+//遍历字符串，统计每组连续相同字符的个数，把个数写入 buf，最后加 '\0' 终止
 int encode_run(const char* sequence, unsigned len, char* buf, unsigned int buf_len) {
-    //TODO
+    unsigned int bi = 0;
+    unsigned int i = 0;
+
+    while (i < len && sequence[i] != '\0') {
+        unsigned int count = 1;
+        while (i + count < len && sequence[i + count] == sequence[i]) {
+            count++;
+        }
+        if (bi < buf_len - 1) {
+            //buf是char类型，所以输出形式要是字符型
+            buf[bi++] = '0' + count;
+        }
+        i += count;
+    }
+    buf[bi] = '\0';
+    return bi;
 }
 
 int main() {
+    //用于存储最后的输出结果
     char encoded_run[128];
     const char* line_run = "1122333334423";
 
