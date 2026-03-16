14709b366fceebacbcc050cf2f2bb52d998b5c67
Kunyu Guo <kguo0343@uni.sydney.edu.au>
2026-03-17 09:47:53 +1100

    Add hamsterdecode exercise

---
+++ Week_3/hamsterdecode/hamster_decode.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX_CODES (10)
+#define MAX_STRING (512)
+
+void decode(const char* data, int length) {
+	//Main function to decode
+	int split = 0;
+	while (split < length && !isdigit(data[split])) split++;
+		
+		if (split > MAX_CODES) return;
+
+		for (int i = split; i < length; i++)
+		{
+			int idx = data[i] - '0';
+			if(idx >= split) return ;
+			putchar(data[idx]);
+		}
+		putchar('\n');
+	}
+
+
+int main() {
+	//Some test statements
+	const char* hlo = "HELOWRD0122343526"; //HELLOWORLD
+	const char* dib = "Dibd aou0121314254313143674367"; //Dibidi ba didi dou dou
+	decode(hlo,strlen(hlo));
+	decode(dib,strlen(dib));
+	return 0;
+}
